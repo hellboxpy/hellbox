641cf0df39d1b29bda0049692a2f69e30ed5e416
Jack Jennings <j@ckjennin.gs>
2014-03-01 19:51:16 -0800

    first commit

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name='Hellbox',
+    version='0.1.0',
+    author='Jack Jennings',
+    author_email='j@ckjennin.gs',
+    packages=['hellbox', 'hellbox.test'],
+    scripts=['bin/hell'],
+    url='http://github.com/jackjennings/Hellbox',
+    license='LICENSE.txt',
+    description='Build system for font development',
+    long_description=open('README.txt').read(),
+    install_requires=[],
+)
