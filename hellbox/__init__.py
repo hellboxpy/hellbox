0e37e181af5ed7f427df349a150ba90e01680b7b
Jack Jennings <j@ckjennin.gs>
2014-06-15 00:37:52 -0700

    make sure hellbox is imported when running the hellfile

--- hellbox/__init__.py
+++ hellbox/__init__.py
@@ -74,7 +74,7 @@ def main():
         run_task(options.task or "default")
 
     def run_task(task):
-        script = 'execfile("Hellfile.py"); Hellbox.run_task("%s")' % task
+        script = 'execfile("Hellfile.py"); import hellbox; hellbox.Hellbox.run_task("%s")' % task
         subprocess.call(['./.hellbox/bin/python', '-c', script])
 
     parser = ArgumentParser(description="""
