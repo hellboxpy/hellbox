ec2ea6783afb5a9ce40ffbcc49e3a144c421f501
Jack Jennings <j@ckjennin.gs>
2014-07-06 21:01:30 -0700

    add source file class

---
+++ hellbox/source_file.py
@@ -0,0 +1,48 @@
+class SourceFile(object):
+
+    def __init__(self):
+        pass
+
+    @property
+    def filename(self):
+        pass
+
+    @filename.setter
+    def filename(self):
+        pass
+
+    @filename.deleter
+    def filename(self):
+        pass
+
+    @property
+    def path(self):
+        pass
+
+    @path.setter
+    def path(self):
+        pass
+
+    @path.deleter
+    def path(self):
+        pass
+
+    @property
+    def extension(self):
+        pass
+
+    @extension.setter
+    def extension(self):
+        pass
+
+    @extension.deleter
+    def extension(self):
+        pass
+
+    @property
+    def source(self):
+        pass
+
+    @source.setter
+    def source(self):
+        pass
