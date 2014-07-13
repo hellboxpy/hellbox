2f5caa0dfc28af0a7f0091edf6f68f901c7275af
Jack Jennings <j@ckjennin.gs>
2014-07-13 16:18:33 -0700

    update example hellfile [skip ci]

--- example/Hellfile.py
+++ example/Hellfile.py
@@ -13,23 +13,19 @@ from packages.test import TestUFO
 from packages.generate_otf import GenerateOTF
 from packages.extension import BuildRoboFontExtension
 
+MakeOTF = Hellbox.compose(TestUFO(),
+                          GenerateOTF(), 
+                          Hellbox.write('otf'))
+
+MakeExt = Hellbox.compose(BuildRoboFontExtension(info_format="yaml"),
+                          Hellbox.write('.'))
+
 with Hellbox('font') as task:
     task.describe('Does a little generation dance.')
-
-    MakeOTF = Hellbox.compose(TestUFO(), GenerateOTF(), Hellbox.write('otf'))
-
     task.read('*.ufo', 'src/*') >> MakeOTF()
 
 with Hellbox('extension') as task:
     task.describe('Builds a robofont extension in place.')
-
-    MakeExt = Hellbox.compose(BuildRoboFontExtension(info_format="yaml"),
-                              Hellbox.write('.'))
-
     task.read('src') >> MakeExt()
-
-task = Task('foobar')
-task.read('*.ufo').to(make_otf)
-Hellbox.add_task(task)
 
 Hellbox.default = 'font'
