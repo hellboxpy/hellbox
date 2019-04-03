ad9557272f1bb988c68735fc8cac33d8333aa696
Jack Jennings <jack@standard-library.com>
2019-04-03 15:17:42 -0700

    Fix Chute.create returning None values

--- tests/mock.py
+++ tests/mock.py
@@ -1,10 +1,12 @@
 class Mock(object):
-    def __init__(self):
+    def __init__(self, returns=None):
         self.called = False
+        self.returns = returns
 
     def __call__(self, *args):
         self.called = True
         self.args = args
+        return self.returns
 
     @property
     def __name__(self):
