aa55d76585efbd1f0e251ddc527b77f7612abc87
Jack Jennings <j@ckjennin.gs>
2026-04-23 19:40:04 -0700

    Update Autoimporter to use importlib.metadata
    Replaces Pipfile.lock JSON parsing with importlib.metadata.distributions(),
    making the autoimporter package-manager-agnostic. Drops the path argument
    from Autoimporter and Hellbox.autoimport().

--- hellbox/autoimporter.py
+++ hellbox/autoimporter.py
@@ -1,18 +1,18 @@
-import json
+import importlib.metadata
 
 
 class Autoimporter(object):
-    def __init__(self, path):
-        self.path = path
-
     def execute(self, globals, locals):
         for mod in self.requirements:
-            imported = __import__(mod, globals, locals, ["*"])
-            globals[mod] = imported
+            try:
+                imported = __import__(mod, globals, locals, ["*"])
+                globals[mod] = imported
+            except ImportError:
+                pass
 
     @property
     def requirements(self):
-        with open(self.path, "r") as f:
-            body = json.load(f)
-            packages = body["default"].keys()
-        return packages
+        return [
+            dist.metadata["Name"].replace("-", "_")
+            for dist in importlib.metadata.distributions()
+        ]
