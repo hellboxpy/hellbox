52bf29ffe8977af6e42107892d2d1bc0b6146e95
Jack Jennings <j@ckjennin.gs>
2026-04-25 19:01:36 -0700

    Rename domain from hellbox.io to hellbox.dev

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -5,6 +5,6 @@ import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://www.hellbox.io",
+  site: "https://www.hellbox.dev",
   integrations: [svelte()],
 });
